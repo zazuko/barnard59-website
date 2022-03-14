3fbbcfbf7730b2ea049e34bc0bd75812a1df6b15
Ludovic Muller <ludovicmuller1@gmail.com>
2022-03-15 00:47:34 +0100

    directly list all packages on collections page

--- pages/package/index.tsx
+++ pages/package/index.tsx
@@ -30,13 +30,26 @@ const Page: NextPage<Props> = ({ lists }) => {
           </Link>
         </Breadcrumbs>
         <h1>Packages collections</h1>
+        <p>This page is listing all package collections.</p>
         {listNames
           .filter((l) => lists[l].packages.length > 0)
           .map((l, index) => {
             return (
-              <p key={index}>
-                <Link href={`/package/${l}`}>{l}</Link>
-              </p>
+              <div key={index}>
+                <Link href={`/package/${l}`} passHref>
+                  <h2 className="capitalize pointer">{l}</h2>
+                </Link>
+                {lists[l].description && <p>{lists[l].description}</p>}
+                <ul>
+                  {lists[l].packages.map((p, index) => {
+                    return (
+                      <li key={index}>
+                        <Link href={`/package/${l}/${p}`}>{p}</Link>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
             );
           })}
       </main>
